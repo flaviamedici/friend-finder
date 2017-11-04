3ecb3de7c7384d63d8a7790d47814334eaf2709a
risaco <rhia.cooper@gmail.com>
2017-11-04 17:34:19 -0500

    updated data in friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,31 +1,71 @@
 // Array that holds data objects, a list of all friends and applicable data
 
-// Might need to change the datatype of answers to string 
+// Might need to change the datatype of answers to string
 module.exports = [
 	{
+		name: "Sansa",
+		photo: "http://images6.fanpop.com/image/polls/1571000/1571738_1467023584408_full.jpg",
+		answers: [3, 1, 1, 1, 5, 4, 1, 4, 4, 5]
+	},
+	{
 		name: "Winnie the Pooh",
-		photo: "https://google.com",
-		answers: [1, 5, 4, 3, 5, 2, 1, 4, 5, 5]
+		photo: "https://pbs.twimg.com/profile_images/691863315853762561/ClkLQse3.jpg",
+		answers: [5, 4, 5, 1, 1, 5, 1, 5, 5, 5]
+	},
+	{
+		name: "Jon",
+		photo: "https://pbs.twimg.com/profile_images/844785536195739651/GkGMChWu.jpg",
+		answers: [4, 1, 2, 1, 5, 5, 1, 5, 5, 5]
+	},
+	{
+		name: "Danaerys",
+		photo: "https://pbs.twimg.com/profile_images/737267065770348544/-_wliDDo.jpg",
+		answers: [5, 1, 4, 1, 5, 5, 1, 5, 5, 5]
+	},
+	{
+		name: "Petyr",
+		photo: "https://d2juyu303oh9b6.cloudfront.net/image/49f3dd7699a17da38e963d5add534749.jpg?&icq=80&sig=577bf959df2f0ac1aa1c1d500ee780a1",
+		answers: [5, 1, 3, 4, 5, 5, 1, 4, 3, 5]
+	},
+	{
+		name: "Margaery",
+		photo: "https://pbs.twimg.com/profile_images/906447699439964161/yZoCaNDC.jpg",
+		answers: [4, 2, 4, 4, 5, 4, 1, 5, 5, 5]
+	},
+	{
+		name: "Jaqen",
+		photo: "https://images-na.ssl-images-amazon.com/images/M/MV5BOGI1ZWZkODQtN2IxMy00NTNjLTk1YmYtOGM4NTk1MTI3M2FkXkEyXkFqcGdeQXVyMjk3NTUyOTc@._V1_.jpg",
+		answers: [4, 1, 1, 1, 3, 1, 1, 5, 2, 5]
+	},
+	{
+		name: "Olenna",
+		photo: "http://i.lv3.hbo.com/assets/images/series/game-of-thrones/character/s7/olenna-tyrell-512x512.jpg",
+		answers: [5, 1, 2, 5, 5, 5, 1, 3, 4, 5]
+	},
+	{
+		name: "Melisandre",
+		photo: "https://assets-auto.rbl.ms/22bd2c7094b5f310574a15e21b55450781a67c19d5ed0ad23b1abdb43b3f820f",
+		answers: [5, 1, 5, 1, 5, 3, 1, 4, 4, 5]
 	},
 	{
 		name: "Elmo",
-		photo: "https://google.com",
-		answers: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+		photo: "https://pbs.twimg.com/profile_images/454270645879070720/82lJeUWO.jpeg",
+		answers: [5, 5, 5, 3, 1, 3, 1, 5, 5, 5]
 	},
 	{
 		name: "Tyrion",
-		photo: "https://google.com",
-		answers: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+		photo: "http://assets.viewers-guide.hbo.com/large5527f803e45b1.jpg",
+		answers: [5, 3, 5, 5, 5, 5, 1, 4, 4, 5]
 	},
 	{
-		name: "Arya",
-		photo: "https://google.com",
-		answers: [2, 5, 4, 2, 5, 2, 5, 4, 5, 5]
+		name: "Drogo",
+		photo: "http://i.lv3.hbo.com/assets/images/series/game-of-thrones/character/s5/khal-drogo-512x512.jpg",
+		answers: [5, 1, 4, 1, 5, 1, 1, 4, 2, 5]
 	},
 	{
-		name: "Cookie Monster",
-		photo: "https://google.com",
-		answers: [5, 5, 4, 3, 5, 5, 5, 4, 5, 5]
+		name: "Jaime",
+		photo: "https://pbs.twimg.com/profile_images/626123344891375616/BYJvXTvy.jpg",
+		answers: [3, 1, 4, 3, 5, 5, 1, 4, 3, 5]
 	}
 ];
 
